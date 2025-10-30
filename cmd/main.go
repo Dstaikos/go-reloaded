59c34687dd7cbafee64845c7b3efd20b827960ea
dstaikos <diogenisstaikos@gmail.com>
2025-10-30 20:14:43 +0200

    1

--- cmd/main.go
+++ cmd/main.go
@@ -8,13 +8,13 @@ import (
 )
 
 func main() {
-	//if len(os.Args) < 3 {
-	//fmt.Println("Usage: go run main.go <input.txt> <output.txt>")
-	//return
-	//}
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run main.go <input.txt> <output.txt>")
+		return
+	}
 
 	inputFile := os.Args[1]
-	// outputFile := os.Args[2]
+	outputFile := os.Args[2]
 
 	data, err := os.ReadFile(inputFile)
 	if err != nil {
@@ -30,6 +30,11 @@ func main() {
 	text = piscine.FixPunctuation(text)
 	text = piscine.AnConvert(text)
 
-	fmt.Println("File contents:")
-	fmt.Println(text)
+	// write processed text to output file
+	if err := os.WriteFile(outputFile, []byte(text), 0o644); err != nil {
+		fmt.Println("Error writing file:", err)
+		return
+	}
+
+	fmt.Println("Wrote output to", outputFile)
 }
